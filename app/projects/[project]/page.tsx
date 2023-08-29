823f61bc9da366480ba42c35501201dd234ad04b
Jacob Mompean <jaymompean@gmail.com>
2023-08-28 20:44:31 -0500

    add getProject and dynamic project page route

---
+++ app/projects/[project]/page.tsx
@@ -0,0 +1,11 @@
+type Props = {
+  params: { project: string };
+};
+
+export default async function Project({ params }: Props) {
+  const slug = params.project;
+
+  const project = await getProject(slug);
+
+  return <div>My project goes here</div>;
+}
--- sanity/sanity-utils.ts
+++ sanity/sanity-utils.ts
@@ -21,3 +21,24 @@ export async function getProjects(): Promise<Project[]> {
     }`
   );
 }
+
+export async function getProject(slug: string): Promise<Project> {
+  const client = createClient({
+    projectId,
+    dataset,
+    apiVersion,
+  });
+
+  return client.fetch(
+    groq`*[_type == 'project' && slug.current == $slug][0]{
+      _id,
+      _createdAt,
+      name,
+      'slug': slug.current,
+      'image': image.asset->url,
+      url,
+      content
+    }`,
+    { slug }
+  );
+}
