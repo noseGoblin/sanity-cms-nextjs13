61eb3b79a1f57872ae2fa0f8d4bb376f909d321c
Jacob Mompean <jaymompean@gmail.com>
2023-08-28 21:14:20 -0500

    add title and button

--- app/projects/[project]/page.tsx
+++ app/projects/[project]/page.tsx
@@ -1,4 +1,5 @@
 import { getProject } from '@/sanity/sanity-utils';
+import Link from 'next/link';
 
 type Props = {
   params: { project: string };
@@ -9,5 +10,26 @@ export default async function Project({ params }: Props) {
 
   const project = await getProject(slug);
 
-  return <div className='min-h-screen '>{project.name}</div>;
+  return (
+    <div className='min-h-screen max-w-3xl mx-auto py-20'>
+      <header className='flex items-center justify-between'>
+        <h1 className='bg-gradient-to-r bg-no-repeat from-orange-400 to-purple-600 bg-clip-text text-transparent text-5xl drop-shadow font-extrabold'>
+          {project.name}
+        </h1>
+        <Link
+          href={project.url}
+          title='View Project'
+          target='_blank'
+          rel='noopener noreferrer'
+          className='bg-gray-100 rounded-lg text-gray-500 font-bold py-3 px-4 whitespace-nowrap hover:bg-pink-500 hover:text-pink-100 transition duration-300 ease-in-out'
+        >
+          View Project
+        </Link>
+      </header>
+
+      {/* content */}
+
+      {/* image */}
+    </div>
+  );
 }
