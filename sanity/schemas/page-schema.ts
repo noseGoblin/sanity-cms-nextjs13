16b145355aa6fdd1933d184b6df8362a4daa4db5
Jacob Mompean <jaymompean@gmail.com>
2023-08-28 22:49:02 -0500

    add page schema

---
+++ sanity/schemas/page-schema.ts
@@ -0,0 +1,29 @@
+const page = {
+  name: 'page',
+  title: 'Pages',
+  type: 'document',
+  field: [
+    {
+      name: 'title',
+      title: 'Title',
+      type: 'string',
+    },
+    {
+      name: 'slug',
+      title: 'Slug',
+      type: 'slug',
+      options: {
+        source: 'title',
+        maxLength: 96,
+      },
+    },
+    {
+      name: 'content',
+      title: 'Content',
+      type: 'array',
+      of: [{ type: 'block' }],
+    },
+  ],
+};
+
+export default page;
